0fd5d8160ed5bb6f438d8d7c21a7d4e11a6ccc58
Marlon <marlon.gayle@live.com>
2023-01-24 16:17:02 -0500

    created base structre for application layer

---
+++ src/content/src/Net7WebApiTemplate.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Net7WebApiTemplate.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services) 
+        {
+            return services;
+        }
+    }
+}
