8373b3bc573bb6255a354fd837731c179e1318c5
Marlon <marlon.gayle@live.com>
2023-01-24 17:45:30 -0500

    remove default project controller and create endpoint for FAQ

---
+++ src/content/src/Net7WebApiTemplate.Api/Endpoints/Faq/FaqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net7WebApiTemplate.Api.Endpoints.Faq
+{
+    [Produces("application/json")]
+    [ApiController]
+    public class FaqController : ControllerBase
+    {
+        public FaqController()
+        {
+        }
+
+        [HttpGet]
+        [Route("api/v1/faqs")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+    }
+}
