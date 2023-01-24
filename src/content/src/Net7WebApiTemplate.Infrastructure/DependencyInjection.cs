5615e634cb11a2bc07fc0ae19cf023547edc078d
Marlon <marlon.gayle@live.com>
2023-01-24 16:37:37 -0500

    Implement dependency injection file

---
+++ src/content/src/Net7WebApiTemplate.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Net7WebApiTemplate.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration,
+            IWebHostEnvironment environment)
+        {
+            return services;
+        }
+    }
+}
