7c831334b9998987a59e856962b510c75c13b138
Marlon <marlon.gayle@live.com>
2023-01-26 13:03:56 -0500

    implement datetime utilities to handle utc  conversions

---
+++ src/content/src/Net7WebApiTemplate.Application/Shared/Utilities/DateTimeUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+
+namespace Net7WebApiTemplate.Application.Shared.Utilities
+{
+    public static class DateTimeUtilities
+    {
+        public static DateTime ConvertDatFromatUtc(DateTime dateTimeUtc, string localTimeZone)
+        {
+            var timeZone = TimeZoneInfo.FindSystemTimeZoneById(localTimeZone);
+            return TimeZoneInfo.ConvertTimeFromUtc(dateTimeUtc, timeZone);
+        }
+    }
+
+    public class TimeZones 
+    {
+        public string Value { get; set; }
+
+        private TimeZones(string values)
+        {
+            Value = values;
+        }
+
+        public static TimeZones CST { get { return new TimeZones("Central Standard Time"); } }
+        public static TimeZones EST { get { return new TimeZones("Eastern Standard Time"); } }
+    }
+}
