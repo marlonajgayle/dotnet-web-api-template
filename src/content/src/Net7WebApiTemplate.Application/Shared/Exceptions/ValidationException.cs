3fa68060d0d7e5d7a98b319e344a7d1e77be604b
Marlon <marlon.gayle@live.com>
2023-01-26 11:00:03 -0500

    Implement validation behaviour using Mediatr

---
+++ src/content/src/Net7WebApiTemplate.Application/Shared/Exceptions/ValidationException.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+
+namespace Net7WebApiTemplate.Application.Shared.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public IDictionary<string, string[]> Errors { get; }
+
+        public ValidationException()
+            :base("One or more validation failures have occured.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+            : this()
+        {
+            Errors = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+    }
+}
