e72705f0c858fdaddc595371a57976c0e8147f96
Marlon <marlon.gayle@live.com>
2023-01-26 10:02:51 -0500

    create logging behavour using Mediatr to log each request

---
+++ src/content/src/Net7WebApiTemplate.Application/Shared/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Net7WebApiTemplate.Application.Shared.Behaviours
+{
+    public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger _logger;
+
+        public LoggingBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogInformation("Net7WebApiTemplate Request: {name}, {@Request}", requestName, request);
+
+            return Task.CompletedTask;
+        }
+    }
+}
