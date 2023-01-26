05225b17c7a1a83f3790f8ca8f2af0e9d4e60059
Marlon <marlon.gayle@live.com>
2023-01-26 12:27:33 -0500

    implementation of unauthorized custom exception

---
+++ src/content/src/Net7WebApiTemplate.Application/Shared/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,10 @@
+﻿namespace Net7WebApiTemplate.Application.Shared.Exceptions
+{
+    public class UnauthorizedException : Exception
+    {
+        public UnauthorizedException(string message)
+        : base(message)
+        {
+        }
+    }
+}
