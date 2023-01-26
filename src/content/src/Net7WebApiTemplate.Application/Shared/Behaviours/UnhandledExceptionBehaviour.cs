f775e0c89dfa1840e68e1abef3bde5011fb32eef
Marlon <marlon.gayle@live.com>
2023-01-26 10:35:12 -0500

    implemement UnhandledExceptionBehaviour using Mediatr

---
+++ src/content/src/Net7WebApiTemplate.Application/Shared/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,35 @@
+﻿
+
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Net7WebApiTemplate.Application.Shared.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try 
+            {
+                return await next();
+            }
+            catch (Exception ex) 
+            {
+                var requestName = typeof(TRequest).Name;
+
+                _logger.LogError(ex, "Net7WebApiTemplate Request: Unhandled Exception for request {Name} {@Request}",
+                    requestName, request);
+
+                throw;
+            }
+        }
+    }
+}
