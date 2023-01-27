fc06cc8cfc044a175d7ce3008db1ef4c746ebe5d
Marlon <marlon.gayle@live.com>
2023-01-27 10:34:42 -0500

    register MediatR pipeline services

--- src/content/src/Net7WebApiTemplate.Application/DependencyInjection.cs
+++ src/content/src/Net7WebApiTemplate.Application/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using Net7WebApiTemplate.Application.Features.HealthChecks;
+using Net7WebApiTemplate.Application.Shared.Behaviours;
+using System.Reflection;
 
 namespace Net7WebApiTemplate.Application
 {
@@ -10,6 +13,12 @@ namespace Net7WebApiTemplate.Application
             // Register Application Health Checks
             services.AddHealthChecks()
                 .AddCheck<ApplicationHealthCheck>(name: "Net7WebApiTemplate API");
+
+            // Register MediatR Services
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour <,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
 
             return services;
         }
