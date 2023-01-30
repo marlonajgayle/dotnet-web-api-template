8fb16bb7203ee39bd8f6853a439d3a4f8691c43d
Marlon <marlon.gayle@live.com>
2023-01-30 13:26:47 -0500

    implemented pagination for database queries

---
+++ src/content/src/Net7WebApiTemplate.Application/Shared/Extensions/PaginationExtension.cs
@@ -0,0 +1,13 @@
+﻿using Net7WebApiTemplate.Application.Shared.Models;
+
+namespace Net7WebApiTemplate.Application.Shared.Extensions
+{
+    public static class PaginationExtension
+    {
+        public static ValueTask<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable,
+            int offset, int limit)
+        {
+            return PaginatedList<TDestination>.CreateAsync(queryable, offset, limit);
+        }
+    }
+}
