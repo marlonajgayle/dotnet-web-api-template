29a1ec1a7ca96fbfa5fb2b11c536046cdf6275b8
Marlon <marlon.gayle@live.com>
2023-01-31 16:30:15 -0500

    Update database context with new entities

--- src/content/src/Net7WebApiTemplate.Application/Shared/Interface/INet7WebApiTemplateDbContext.cs
+++ src/content/src/Net7WebApiTemplate.Application/Shared/Interface/INet7WebApiTemplateDbContext.cs
@@ -6,7 +6,9 @@ namespace Net7WebApiTemplate.Application.Shared.Interface
     public interface INet7WebApiTemplateDbContext
     {
         DbSet<Faq> Faqs { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<ProductCatergory> ProductCatergories { get;set; }
 
-        
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
--- src/content/src/Net7WebApiTemplate.Persistence/Net7WebApiTemplateDbContext.cs
+++ src/content/src/Net7WebApiTemplate.Persistence/Net7WebApiTemplateDbContext.cs
@@ -7,11 +7,18 @@ namespace Net7WebApiTemplate.Persistence
     public class Net7WebApiTemplateDbContext : DbContext, INet7WebApiTemplateDbContext
     {
         public DbSet<Faq> Faqs { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCatergory> ProductCatergories { get; set; }
 
         public Net7WebApiTemplateDbContext(DbContextOptions<Net7WebApiTemplateDbContext> options)
             : base(options)
         {
 
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return base.SaveChangesAsync(cancellationToken);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -20,5 +27,6 @@ namespace Net7WebApiTemplate.Persistence
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(Net7WebApiTemplateDbContext).Assembly);
             base.OnModelCreating(modelBuilder);
         }
+        
     }
 }
