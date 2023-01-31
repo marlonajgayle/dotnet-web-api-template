98743a3bcbbb8d53f7ed0705d852e055e8cd908e
Marlon <marlon.gayle@live.com>
2023-01-31 17:11:28 -0500

    implement fluent validation for product commands

---
+++ src/content/src/Net7WebApiTemplate.Application/Features/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Net7WebApiTemplate.Application.Features.Products.Commands.CreateProduct
+{
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator() 
+        {
+            RuleFor(v => v.ProductName)
+                .NotEmpty().WithMessage("Product name field is required!")
+                .MaximumLength(100).WithMessage("Product name has a maximum field size of 100 characters.");
+
+            RuleFor(v => v.ProductDescription)
+                .NotEmpty().WithMessage("Product description field is required!")
+                .MaximumLength(100).WithMessage("Product description has a maximum field size of 500 characters.");
+
+            RuleFor(v => v.ProductPrice)
+                .NotEmpty().WithMessage("Product price field is required!")
+                .GreaterThan(0).WithMessage("");
+        }
+    }
+}
---
+++ src/content/src/Net7WebApiTemplate.Application/Features/Products/Commands/DeleteProduct/DeleteProductCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Net7WebApiTemplate.Application.Features.Products.Commands.DeleteProduct
+{
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotEmpty().WithMessage("Id field is required.")
+                .GreaterThan(0).WithMessage("Id value must be greater than zero.");
+        }
+    }
+}
