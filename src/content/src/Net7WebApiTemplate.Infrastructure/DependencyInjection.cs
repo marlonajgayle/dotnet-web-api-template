56809b2cb6238dfe45460dff986d50d6b8403621
Marlon <marlon.gayle@live.com>
2023-01-31 17:26:28 -0500

    Implement InMemory Cache provider

--- src/content/src/Net7WebApiTemplate.Infrastructure/DependencyInjection.cs
+++ src/content/src/Net7WebApiTemplate.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Net7WebApiTemplate.Application.Shared.Interface;
+using Net7WebApiTemplate.Infrastructure.Cache.InMemory;
 
 namespace Net7WebApiTemplate.Infrastructure
 {
@@ -9,6 +11,10 @@ namespace Net7WebApiTemplate.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration,
             IWebHostEnvironment environment)
         {
+            // Register InMemory Cache
+            services.AddMemoryCache();
+            services.AddSingleton<ICacheProvider, InMemoryCacheProvider> ();
+
             return services;
         }
     }
