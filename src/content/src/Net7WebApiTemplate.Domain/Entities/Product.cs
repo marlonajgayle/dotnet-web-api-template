3f2f0f121bc6393705404e213ee1907b84523180
Marlon <marlon.gayle@live.com>
2023-01-31 16:22:09 -0500

    Added product and product category entities

---
+++ src/content/src/Net7WebApiTemplate.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Net7WebApiTemplate.Domain.Entities
+{
+    public class Product
+    {
+        public long ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public decimal ProductPrice { get; set; }
+        public ProductCatergory ProductCatergory { get; set; } = new ProductCatergory();
+
+    }
+}
---
+++ src/content/src/Net7WebApiTemplate.Domain/Entities/ProductCatergory.cs
@@ -0,0 +1,10 @@
+﻿namespace Net7WebApiTemplate.Domain.Entities
+{
+    public class ProductCatergory
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string CategoryDescription { get; set; } = string.Empty;
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
