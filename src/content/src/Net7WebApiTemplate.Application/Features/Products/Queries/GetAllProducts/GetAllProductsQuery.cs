37ef71d91896f359c6f2726bb5cb5dbc783ff7ae
Marlon <marlon.gayle@live.com>
2023-01-31 16:29:53 -0500

    implement product query

---
+++ src/content/src/Net7WebApiTemplate.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQuery.cs
@@ -0,0 +1,41 @@
+﻿using Mediator;
+using Microsoft.EntityFrameworkCore;
+using Net7WebApiTemplate.Application.Shared.Extensions;
+using Net7WebApiTemplate.Application.Shared.Interface;
+using Net7WebApiTemplate.Application.Shared.Models;
+
+namespace Net7WebApiTemplate.Application.Features.Products.Queries.GetAllProducts
+{
+    public record GetAllProductsQuery : IRequest<PaginatedList<ProductsDto>>
+    {
+        public int Offset { get; set; } 
+        public int Limit { get; set; }
+    }
+
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, PaginatedList<ProductsDto>>
+    {
+        private readonly INet7WebApiTemplateDbContext _dbContext;
+
+        public GetAllProductsQueryHandler(INet7WebApiTemplateDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async ValueTask<PaginatedList<ProductsDto>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _dbContext.Products
+                .AsNoTracking()
+                .Select(p => new ProductsDto
+                {
+                    Id = p.ProductId,
+                    ProductName = p.ProductName,
+                    ProductDescription = p.ProductDescription,
+                    Price = p.ProductPrice
+                })
+                .OrderBy(p => p.ProductName)
+                .PaginatedListAsync(request.Offset, request.Limit, cancellationToken);
+
+            return products;
+        }
+    }
+}
---
+++ src/content/src/Net7WebApiTemplate.Application/Features/Products/Queries/GetAllProducts/ProductsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Net7WebApiTemplate.Application.Features.Products.Queries.GetAllProducts
+{
+    public class ProductsDto
+    {
+        public long Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
