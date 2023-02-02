5bed5d2f2e4a123cb39552d09f6cc55399695325
Marlon <marlon.gayle@live.com>
2023-02-02 17:19:08 -0500

    Implement Update Product Command

---
+++ src/content/src/Net7WebApiTemplate.Application/Features/Products/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Net7WebApiTemplate.Domain.Entities;
+
+namespace Net7WebApiTemplate.Application.Features.Products.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetById(long id);
+        Task Update(Product product);
+    }
+}
