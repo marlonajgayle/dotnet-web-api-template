e50d55e2bd71557d2d5c97e5ffa372b7d1483e2a
Marlon <marlon.gayle@live.com>
2023-02-03 11:25:05 -0500

    Implement Email sender service along with email template

--- src/content/src/Net7WebApiTemplate.Infrastructure/DependencyInjection.cs
+++ src/content/src/Net7WebApiTemplate.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Net7WebApiTemplate.Application.Shared.Interface;
 using Net7WebApiTemplate.Infrastructure.ApiClients.GitHub;
 using Net7WebApiTemplate.Infrastructure.Cache.InMemory;
 using Net7WebApiTemplate.Infrastructure.DataProtection;
+using Net7WebApiTemplate.Infrastructure.Email;
 using Polly;
+using System.Net;
+using System.Net.Mail;
 
 namespace Net7WebApiTemplate.Infrastructure
 {
@@ -14,10 +18,25 @@ namespace Net7WebApiTemplate.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration,
             IWebHostEnvironment environment)
         {
-
+            //
             // Register Data Protection
             services.AddDataProtection();
             services.AddSingleton<IEncryptionManager, EncryptionManager>();
+
+            // Register Email Sender and Configurations
+            var emailSenderOptions = new EmailSenderOptions();
+            configuration.GetSection(nameof(EmailSenderOptions)).Bind(emailSenderOptions);
+            services.AddScoped<IEmailSender, EmailSenderService>();
+
+            services.AddFluentEmail(defaultFromEmail: emailSenderOptions.FromEmail)
+                .AddRazorRenderer()
+                .AddSmtpSender(new SmtpClient(emailSenderOptions.Host, emailSenderOptions.Port)
+                {
+                    DeliveryMethod = SmtpDeliveryMethod.Network ,
+                    Credentials = (emailSenderOptions.RequiresAuthentication) ? 
+                    new NetworkCredential(emailSenderOptions.Username, emailSenderOptions.Password) : null,
+                    EnableSsl = emailSenderOptions.EnableSsl
+                });
 
             // Register Http Client
             services.AddHttpClient(name: "GitHub", client =>
