dcb664d506b0b5d9f04c6b0c3de7215b0a825e17
Marlon <marlon.gayle@live.com>
2023-02-05 13:07:02 -0500

    create interfaces for authentication and jwt services.

---
+++ src/content/src/Net7WebApiTemplate.Application/Features/Authentication/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using Net7WebApiTemplate.Application.Features.Authentication.Models;
+
+namespace Net7WebApiTemplate.Application.Features.Authentication.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<Result> PasswordSignInAsync(string email, string password, bool LockoutOnFailure);
+    }
+}
---
+++ src/content/src/Net7WebApiTemplate.Application/Features/Authentication/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace Net7WebApiTemplate.Application.Features.Authentication.Interfaces
+{
+    public interface IJwtTokenService
+    {
+        Task<AuthenticationResult> GenerateClaimsTokenAsync(string username, CancellationToken cancellationToken);
+        Task<ClaimsPrincipal> GetPrincipFromTokenAsync(string token);
+    }
+}
