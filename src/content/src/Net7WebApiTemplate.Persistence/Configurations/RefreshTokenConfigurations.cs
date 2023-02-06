1b67262d6babed99e25835d262cefb052e8ee7eb
Marlon <marlon.gayle@live.com>
2023-02-06 11:04:03 -0500

    add refreshtoken ef core configurations

---
+++ src/content/src/Net7WebApiTemplate.Persistence/Configurations/RefreshTokenConfigurations.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Net7WebApiTemplate.Domain.Entities;
+
+namespace Net7WebApiTemplate.Persistence.Configurations
+{
+    public class RefreshTokenConfigurations : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.Property(e => e.Id)
+                .HasColumnName("RefreshTokenID")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(e => e.UserId)
+                .HasMaxLength(128)
+                .IsRequired();
+
+            builder.Property(e => e.JwtId)
+                .HasMaxLength(128)
+                .IsRequired();
+
+            builder.Property(e => e.Token)
+                .HasMaxLength(128)
+                .IsRequired();
+
+            builder.Property(e => e.CreationDate)
+                .IsRequired();
+
+            builder.Property(e => e.ExpirationDate)
+                .IsRequired();
+
+            builder.Property(e => e.Revoked);
+        }
+    }
+}
