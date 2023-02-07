2c81d176e06e4159db4d2e3363ddea4cefa77651
Marlon <marlon.gayle@live.com>
2023-02-07 15:42:13 -0500

    create jwtTokenService and TokenResult class

--- src/content/src/Net7WebApiTemplate.Application/Features/Authentication/Interfaces/IJwtTokenService.cs
+++ src/content/src/Net7WebApiTemplate.Application/Features/Authentication/Interfaces/IJwtTokenService.cs
@@ -1,10 +1,12 @@
-﻿using System.Security.Claims;
+﻿using Net7WebApiTemplate.Application.Features.Authentication.Models;
+using System.Security.Claims;
 
 namespace Net7WebApiTemplate.Application.Features.Authentication.Interfaces
 {
     public interface IJwtTokenService
     {
-        Task<AuthenticationResult> GenerateClaimsTokenAsync(string username, CancellationToken cancellationToken);
-        Task<ClaimsPrincipal> GetPrincipFromTokenAsync(string token);
+        Task<TokenResult> GenerateClaimsTokenAsync(string username, CancellationToken cancellationToken);
+        Task<ClaimsPrincipal?> GetPrincipFromTokenAsync(string token);
+        Task<TokenResult> RefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken);
     }
 }
