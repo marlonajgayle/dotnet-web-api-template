91fb7adbb703fde1b0df004a6868af677ce110ef
Marlon <marlon.gayle@live.com>
2023-02-16 17:46:52 -0500

    Update login endpoint method

---
+++ src/content/src/Net7WebApiTemplate.Api/Endpoints/Auth/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Net7WebApiTemplate.Api.Endpoints.Auth
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
