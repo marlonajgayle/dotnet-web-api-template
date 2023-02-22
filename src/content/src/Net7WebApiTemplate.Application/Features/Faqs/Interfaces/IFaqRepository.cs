f161da665b269f59a44addd6f999a4a9bea3679e
Marlon <marlon.gayle@live.com>
2023-02-22 14:50:13 -0500

    create inteface for Faq repository

---
+++ src/content/src/Net7WebApiTemplate.Application/Features/Faqs/Interfaces/IFaqRepository.cs
@@ -0,0 +1,12 @@
+﻿using Net7WebApiTemplate.Domain.Entities;
+
+namespace Net7WebApiTemplate.Application.Features.Faqs.Interfaces
+{
+    public interface IFaqRepository
+    {
+        Task Add(Faq faq);
+        Task<Faq> GetFaqByIdAsync(int id);
+        Task<IEnumerable<Faq>> GetAll(int offset, int limit);
+        Task Remove(int id);
+    }
+}
