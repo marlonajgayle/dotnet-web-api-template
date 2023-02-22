af3da7fe887a4d5e5117964c90b5750a29cc4ad9
Marlon <marlon.gayle@live.com>
2023-02-22 14:51:25 -0500

    Create SQLConnection factory to handle sql connection for Dapper

---
+++ src/content/src/Net7WebApiTemplate.Persistence/SqlConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Net7WebApiTemplate.Persistence
+{
+    public sealed class SqlConnectionFactory : ISqlConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_configuration.GetConnectionString("DatabaseConnection"));
+        }
+    }
+}
