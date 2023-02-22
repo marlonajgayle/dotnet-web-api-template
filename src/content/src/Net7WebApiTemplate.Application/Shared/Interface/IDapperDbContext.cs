4bfb83af2104301cbdc0d3703918ce1cefeaba3c
Marlon <marlon.gayle@live.com>
2023-02-22 15:15:05 -0500

    remove DapperDbContext as SqlConnectionFactrory is now being used.

--- src/content/src/Net7WebApiTemplate.Application/Shared/Interface/IDapperDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Data.SqlClient;
-
-namespace Net7WebApiTemplate.Application.Shared.Interface
-{
-    public interface IDapperDbContext
-    {
-        SqlConnection CreateConnection();
-    }
-}
