2dabe393c7b338135c1703b85f85798a8bcd5411
Marlon <marlon.gayle@live.com>
2023-03-04 09:10:33 -0500

    Implement logic for creating  identity role

---
+++ src/content/src/Net7WebApiTemplate.Application/Features/Authentication/Commands/CreateRole/CreateRoleCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Net7WebApiTemplate.Application.Features.Authentication.Commands.CreateRole
+{
+    public class CreateRoleCommandValidator : AbstractValidator<CreateRoleCommand>
+    {
+        public CreateRoleCommandValidator()
+        {
+            RuleFor(v => v.RoleName)
+                .NotEmpty().WithMessage("Role Name field is required.");
+        }
+    }
+}
