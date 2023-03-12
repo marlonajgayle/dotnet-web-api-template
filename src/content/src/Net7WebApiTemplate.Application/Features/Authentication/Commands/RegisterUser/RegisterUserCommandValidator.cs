f130f3483be914c022e2a05d7a289bacaec578b4
Marlon <marlon.gayle@live.com>
2023-03-12 16:54:47 -0500

    implement validator for registration command

---
+++ src/content/src/Net7WebApiTemplate.Application/Features/Authentication/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Net7WebApiTemplate.Application.Features.Authentication.Commands.RegisterUser
+{
+    public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(v => v.FirstName)
+                .NotEmpty().WithMessage("Firstname field is required.");
+
+            RuleFor(v => v.LastName)
+                .NotEmpty().WithMessage("Lastname field is required.");
+
+            RuleFor(v => v.Email).Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("Email field is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(v => v.Password)
+                .NotEmpty().WithMessage("Password field is required.");
+        }
+    }
+}
