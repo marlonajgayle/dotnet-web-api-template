e87666572f52380b8f76e3a59270923762cffc2d
Marlon <marlon.gayle@live.com>
2023-08-08 12:26:28 -0500

    Create auditable entity template and files for Frontier integration

---
+++ src/content/.frontier/template/auditable-entity/auditable-entity.cs
@@ -0,0 +1,12 @@
+﻿using __PROJECT_NAME__.Domain.Shared;
+
+namespace __PROJECT_NAME__.Domain.Entities
+{
+    public class __ENTITY_NAME__ : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Property1 { get; set; } = String.Empty;
+        public string Property2 { get; set; } = String.Empty;
+        public string Property3 { get; set; } = String.Empty;
+    }
+}
